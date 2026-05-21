b340a16de298c1f480dc2feb1a820cb2295042fc
clen0011-cell <clen0011@student.monash.edu>
2026-05-21 08:34:09 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,7 @@ vegaEmbed('#choropleth',     'vega/choropleth.json',     embedOpts);
 vegaEmbed('#barchart',       'vega/barchart.json',       embedOpts);
 vegaEmbed('#treemap',        'vega/treemap.json',        embedOpts);
 vegaEmbed('#heatmap',        'vega/heatmap.json',        embedOpts);
-vegaEmbed('#smallmultiples', 'vega/smallmultiples.json', {
-  actions: false,
-  renderer: 'svg',
-  width: 700
-});
+vegaEmbed('#smallmultiples', 'vega/smallmultiples.json', {actions: false, renderer: 'svg', width: 850});
 vegaEmbed('#areachart',      'vega/areachart.json',      embedOpts);
 vegaEmbed('#dumbbell',       'vega/dumbbell.json',       embedOpts);
 vegaEmbed('#stackedbar',     'vega/stackedbar.json',     embedOpts);
